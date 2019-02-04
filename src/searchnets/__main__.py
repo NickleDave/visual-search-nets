dc44143a9d0b137b8d0acc84ec57e8769bcceb8a
NickleDave <nickledave@users.noreply.github.com>
2019-02-04 14:14:30 -0500

    initial add of src/searchnets/__main__.py

---
+++ src/searchnets/__main__.py
@@ -0,0 +1,78 @@
+"""
+Invokes __main__ when the module is run as a script.
+Example: python -m searchnets --help
+The package is installed on the path by pip, so typing
+`$ searchnets --help` would have the same effect (i.e., no need
+to type the python -m)
+"""
+import os
+import sys
+import argparse
+from datetime import datetime
+import logging
+import importlib
+import importlib.util
+from configparser import ConfigParser
+import json
+
+import tensorflow as tf
+
+
+def cli(command, configfile):
+    """command-line interface
+    Called by main() when user runs ram from the command-line by typing 'searchnets'
+
+    Parameters
+    ----------
+    command : str
+        Command to follow. One of {'train', 'test'}
+            Train : train models using configuration defined in config file.
+            Test : test accuracy of trained models using configuration defined in configfile.
+
+    configfile : str
+        Path to a `config.ini` file that defines the configuration.
+
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+    >>> cli(command='train', config='./configs/quick_run_config.ini')
+
+    Notes
+    -----
+    This function is not really meant to be run by the user, but has its own arguments
+    to make it easier to test (instead of throwing everything into one 'main' function)
+    """
+    # get config first so we can know if we should save log, where to make results directory, etc.
+    config = ram.parse_config(configfile)
+    if command == 'data':
+        data(config)
+    elif command == 'train':
+        train(config)
+    elif command == 'test':
+        test(config)
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='main script',
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('command', type=str, choices=['data', 'train', 'test'],
+                        help="Command to run, either 'data', 'train' or 'test' \n"
+                             "$ searchstims train ./configs/config_2018-12-17.ini")
+    parser.add_argument('configfile', type=str,
+                        help='name of config.ini file to use \n'
+                             '$ searchstims train ./configs/config_2018-12-17.ini')
+    return parser
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    cli(command=args.command,
+        configfile=args.configfile)
+
+
+if __name__ == '__main__':
+    main()
