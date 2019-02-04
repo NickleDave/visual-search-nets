ac990d2f83130cdc70e23d4fc1e1cc388d089c5a
nickledave <nickledave@users.noreply.github.com>
2019-02-03 22:35:26 -0500

    add plots/ sub-package to src/searchnets, with figures.py

---
+++ src/searchnets/plots/figures.py
@@ -0,0 +1,39 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+SET_SIZES = [1, 2, 4, 8]
+
+
+mpl.style.use('bmh')
+
+plt.rcParams['font.size'] = 18
+plt.rcParams['axes.labelsize'] = 18
+plt.rcParams['axes.labelweight'] = 'regular'
+plt.rcParams['axes.titlesize'] = 20
+plt.rcParams['xtick.labelsize'] = 16
+plt.rcParams['ytick.labelsize'] = 16
+plt.rcParams['legend.fontsize'] = 16
+plt.rcParams['figure.titlesize'] = 20
+
+
+def plot_results(eff_accs, ineff_accs, epochs, savefig=False, savedir=None):
+    fig, ax = plt.subplots(1, 2, sharey=True)
+    fig.set_size_inches(10, 5)
+    ax = ax.ravel()
+    ax[0].plot(SET_SIZES, eff_accs.T)
+    ax[0].set_xticks(SET_SIZES)
+    ax[0].set_title('efficient')
+    ax[0].set_xlabel('set size')
+    ax[0].set_ylabel('accuracy')
+
+    ax[1].plot(SET_SIZES, ineff_accs.T)
+    ax[1].set_xticks(SET_SIZES)
+    ax[1].set_title('inefficient')
+    ax[1].set_xlabel('set size')
+    ax[1].set_ylim([0, 1.1])
+
+    plt.tight_layout()
+
+    if savefig:
+        fname = os.path.join(savedir, f'alexnet_eff_v_ineff_{epochs}_epochs.png')
+        plt.savefig(fname)
