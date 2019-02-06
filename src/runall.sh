b0b305e0b7078cab14bc7742c87cb00cefcd621a
NickleDave <nickledave@users.noreply.github.com>
2019-02-05 21:20:52 -0500

    rewrite runall.sh with full paths to config files

--- src/runall.sh
+++ src/runall.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 
 # create visual search stimuli used to train neural nets
-searchstims config_number_082818.ini
-searchstims config_rectangle_082818.ini
+searchstims ./configs/searchstims/config_number_082818.ini
+searchstims ./configs/searchstims/config_rectangle_082818.ini
 
 # prepare data sets for training neural nets
-searchnets data config_082918_efficient_10_epochs.ini
-searchnets data config_082918_efficient_400_epochs.ini
-searchnets data config_082918_inefficient_10_epochs.ini
-searchnets data config_082918_inefficient_400_epochs.ini
+searchnets data ./configs/searchnets/config_082918_efficient_10_epochs.ini
+searchnets data ./configs/searchnets/config_082918_efficient_400_epochs.ini
+searchnets data ./configs/searchnets/config_082918_inefficient_10_epochs.ini
+searchnets data ./configs/searchnets/config_082918_inefficient_400_epochs.ini
 
 # train neural nets
-searchnets train config_082918_efficient_10_epochs.ini
-searchnets train config_082918_efficient_400_epochs.ini
-searchnets train config_082918_inefficient_10_epochs.ini
-searchnets train config_082918_inefficient_400_epochs.ini
+searchnets train ./configs/searchnets/config_082918_efficient_10_epochs.ini
+searchnets train ./configs/searchnets/config_082918_efficient_400_epochs.ini
+searchnets train ./configs/searchnets/config_082918_inefficient_10_epochs.ini
+searchnets train ./configs/searchnets/config_082918_inefficient_400_epochs.ini
+
+# test
+searchnets test ./configs/searchnets/config_082918_efficient_10_epochs.ini
+searchnets test ./configs/searchnets/config_082918_efficient_400_epochs.ini
+searchnets test ./configs/searchnets/config_082918_inefficient_10_epochs.ini
+searchnets test ./configs/searchnets/config_082918_inefficient_400_epochs.ini
+
