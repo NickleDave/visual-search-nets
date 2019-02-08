64c023d4685e0d96f30206b11654bc98662afd6e
David Nicholson <nicholdav@gmail.com>
2019-02-07 19:12:40 -0500

    remove dates from names of .ini files in runall.sh to match renamed files

--- src/runall.sh
+++ src/runall.sh
@@ -3,24 +3,23 @@
 source ~/anaconda3/bin/activate searchnets
 
 # create visual search stimuli used to train neural nets
-searchstims ./configs/searchstims/config_number_082818.ini
-searchstims ./configs/searchstims/config_rectangle_082818.ini
+searchstims ./configs/searchstims/config_efficient.ini
+searchstims ./configs/searchstims/config_inefficient.ini
 
 # prepare data sets for training neural nets
-searchnets data ./configs/searchnets/config_082918_efficient_10_epochs.ini
-searchnets data ./configs/searchnets/config_082918_efficient_400_epochs.ini
-searchnets data ./configs/searchnets/config_082918_inefficient_10_epochs.ini
-searchnets data ./configs/searchnets/config_082918_inefficient_400_epochs.ini
+# only need to do this once for efficient + inefficient stimuli
+searchnets data ./configs/searchnets/config_efficient_10_epochs.ini
+searchnets data ./configs/searchnets/config_inefficient_10_epochs.ini
 
 # train neural nets
-searchnets train ./configs/searchnets/config_082918_efficient_10_epochs.ini
-searchnets train ./configs/searchnets/config_082918_efficient_400_epochs.ini
-searchnets train ./configs/searchnets/config_082918_inefficient_10_epochs.ini
-searchnets train ./configs/searchnets/config_082918_inefficient_400_epochs.ini
+searchnets train ./configs/searchnets/config_efficient_10_epochs.ini
+searchnets train ./configs/searchnets/config_efficient_400_epochs.ini
+searchnets train ./configs/searchnets/config_inefficient_10_epochs.ini
+searchnets train ./configs/searchnets/config_inefficient_400_epochs.ini
 
 # test
-searchnets test ./configs/searchnets/config_082918_efficient_10_epochs.ini
-searchnets test ./configs/searchnets/config_082918_efficient_400_epochs.ini
-searchnets test ./configs/searchnets/config_082918_inefficient_10_epochs.ini
-searchnets test ./configs/searchnets/config_082918_inefficient_400_epochs.ini
+searchnets test ./configs/searchnets/config_efficient_10_epochs.ini
+searchnets test ./configs/searchnets/config_efficient_400_epochs.ini
+searchnets test ./configs/searchnets/config_inefficient_10_epochs.ini
+searchnets test ./configs/searchnets/config_inefficient_400_epochs.ini
 
