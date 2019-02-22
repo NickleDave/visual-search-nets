685b61bf87f440f1b12a7cc02232feb4e9796376
David Nicholson <nicholdav@gmail.com>
2019-02-22 17:43:02 -0500

    rewrite searchnets/train.py, more readable, less magic

--- src/searchnets/train.py
+++ src/searchnets/train.py
@@ -7,7 +7,8 @@ import tensorflow as tf
 import joblib
 
 from .alexnet import AlexNet
-from .utils.net.raschka_tf_utils import train as _train
+from .vgg16 import VGG16
+from .utils.net.raschka_tf_utils import batch_generator
 from .utils.net.raschka_tf_utils import save
 
 
@@ -39,42 +40,43 @@ def train(config):
         val_set = None
 
     # boilerplate to unpack config from TRAIN section
-    net_name = int(config['TRAIN']['NETNAME'])
+    net_name = config['TRAIN']['NETNAME']
     number_nets_to_train = int(config['TRAIN']['number_nets_to_train'])
     input_shape = ast.literal_eval(config['TRAIN']['INPUT_SHAPE'])
     new_learn_rate_layers = ast.literal_eval(config['TRAIN']['NEW_LEARN_RATE_LAYERS'])
     base_learning_rate = float(config['TRAIN']['BASE_LEARNING_RATE'])
     new_layer_learning_rate = float(config['TRAIN']['NEW_LAYER_LEARNING_RATE'])
     epochs = int(config['TRAIN']['EPOCHS'])
-    random_seed = int(config['TRAIN']['RANDOM_SEED'])
     batch_size = int(config['TRAIN']['BATCH_SIZE'])
+    random_seed = int(config['TRAIN']['RANDOM_SEED'])
+
+    np.random.seed(random_seed)  # for shuffling in batch_generator
+    tf.random.set_random_seed(random_seed)
 
     for net_number in range(number_nets_to_train):
         tf.reset_default_graph()
-        with tf.Session() as sess:
-            graph = tf.Graph()
-            x = tf.placeholder(tf.float32, (None,) + alexnet_input_shape, name='x')
+        graph = tf.Graph()
+        with tf.Session(graph=graph) as sess:
+            x = tf.placeholder(tf.float32, (None,) + input_shape, name='x')
             y = tf.placeholder(tf.int32, shape=[None], name='y')
             y_onehot = tf.one_hot(indices=y, depth=len(np.unique(y_train)),
                                   dtype=tf.float32, name='y_onehot')
 
-            if net_name == 'AlexNet':
-                dropout_rate_placeholder = tf.placeholder(tf.float32, name='dropout_rate')
-                model = AlexNet(x, init_layer=new_learn_rate_layers, dropout_rate=dropout_rate_placeholder)
+            if net_name == 'alexnet':
+                rate = tf.placeholder_with_default(tf.constant(1.0, dtype=tf.float32), shape=(), name='dropout_rate')
+                model = AlexNet(x, init_layer=new_learn_rate_layers, dropout_rate=rate)
                 dropout_rate = 0.5
             elif net_name == 'VGG16':
-                dropout_rate = None
+                model = VGG16()
+                dropout_rate = 1.0
 
             predictions = {
-                'probabilities': tf.nn.softmax(layers_list[-1],  # last fully-connected
-                                               name='probabilities'),
-                'labels': tf.cast(tf.argmax(layers_list[-1], axis=1),
-                                  tf.int32,
-                                  name='labels')
+                'probabilities': tf.nn.softmax(model.output, name='probabilities'),
+                'labels': tf.cast(tf.argmax(model.output, axis=1), tf.int32, name='labels')
             }
 
             cross_entropy_loss = tf.reduce_mean(
-                tf.nn.softmax_cross_entropy_with_logits(logits=layers_list[-1],
+                tf.nn.softmax_cross_entropy_with_logits(logits=model.output,
                                                         labels=y_onehot),
                 name='cross_entropy_loss')
 
@@ -104,15 +106,37 @@ def train(config):
                 tf.cast(correct_predictions, tf.float32),
                 name='accuracy')
 
-            _train(sess,
-                   training_set=training_set,
-                   validation_set=val_set,
-                   initialize=True,
-                   epochs=epochs,
-                   shuffle=True,
-                   random_seed=random_seed,
-                   batch_size=batch_size,
-                   droput_rate=dropout_rate)
+            X_data = np.array(training_set[0])
+            y_data = np.array(training_set[1])
+            training_loss = []
+
+            sess.run(tf.global_variables_initializer())
+
+            for epoch in range(1, epochs + 1):
+                batch_gen = batch_generator(X_data, y_data,
+                                            batch_size=batch_size,
+                                            shuffle=True)
+                avg_loss = 0.0
+                for i, (batch_x, batch_y) in enumerate(batch_gen):
+                    feed = {x: batch_x,
+                            y: batch_y,
+                            rate: dropout_rate}
+
+                    loss, _ = sess.run(
+                        [cross_entropy_loss, train_op],
+                        feed_dict=feed)
+                    avg_loss += loss
+
+                training_loss.append(avg_loss / (i + 1))
+                print('Epoch %02d Training Avg. Loss: %7.3f' % (
+                    epoch, avg_loss), end=' ')
+                if val_set is not None:
+                    feed = {x: val_set[0],
+                            y: val_set[1]}
+                    valid_acc = sess.run(accuracy, feed_dict=feed)
+                    print(' Validation Acc: %7.3f' % valid_acc)
+                else:
+                    print()
 
             savepath = os.path.join(config['TRAIN']['MODEL_SAVE_PATH'],
                                     'net_number_{}'.format(net_number))
