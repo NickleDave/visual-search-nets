6a173f2dde772012b0008d836b5b1c0d06f32bed
David Nicholson <nicholdav@gmail.com>
2019-02-22 19:23:31 -0500

    layers.py contains layers used by AlexNet and VGG16

---
+++ src/searchnets/nets/layers.py
@@ -0,0 +1,23 @@
+"""convenience functions for creating neural network layers"""
+import tensorflow as tf
+
+
+def max_pool(x, filter_height, filter_width, stride_y, stride_x, name,
+             padding='SAME'):
+    """Create a max pooling layer."""
+    return tf.nn.max_pool(x, ksize=[1, filter_height, filter_width, 1],
+                          strides=[1, stride_y, stride_x, 1],
+                          padding=padding, name=name)
+
+
+def lrn(x, radius, alpha, beta, name, bias=1.0):
+    """Create a local response normalization layer."""
+    return tf.nn.local_response_normalization(x, depth_radius=radius,
+                                              alpha=alpha, beta=beta,
+                                              bias=bias, name=name)
+
+
+
+def dropout(x, dropout_rate):
+    """Create a dropout layer."""
+    return tf.nn.dropout(x, dropout_rate)
