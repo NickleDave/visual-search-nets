9c4e28b662ea056c4cbd925ab520e5f21cb80649
David Nicholson <nicholdav@gmail.com>
2019-03-01 15:52:10 -0500

    add make_targz and targz_dirs functions to searchstims.utils.general

--- src/searchnets/utils/general.py
+++ src/searchnets/utils/general.py
@@ -1,4 +1,6 @@
 import csv
+import tarfile
+import os
 
 import pandas
 from statsmodels.formula.api import ols
@@ -50,3 +52,33 @@ def anova(csv_fname):
     model = ols('acc ~ C(stim_type) + epochs + set_size', data).fit()
     print(model.summary())
     return model
+
+
+def make_targz(output_filename, source_dir):
+    """make a .tar.gz file from a directory, i.e. a gzip-compressed archive"""
+    with tarfile.open(name=output_filename, mode="w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
+
+
+def targz_dirs(dir_names=('checkpoints', 'configs', 'data_prepd_for_nets', 'results', 'visual_search_stimuli'),
+               path='.'):
+    """makes a .tar.gz archive for a list of directories, for uploading to figshare.
+
+    Parameters
+    ----------
+    dir_names : list
+        of directory names.
+        Default is ['checkpoints', 'configs', 'data_prepd_for_nets', 'results', 'visual_search_stimuli'].
+    path : str
+        Path to parent directory where all the directories in dir_names can be found.
+        Default is '.', i.e. current working directory.
+
+    Notes
+    -----
+    Basically just a wrapper around make_targz for convenience;
+    assumes each .tar.gz output should have the name 'directory_name.tar.gz'
+    """
+    for dir_name in dir_names:
+        output_filename = dir_name + '.tar.gz'
+        print(f'making {output_filename}')
+        make_targz(output_filename=output_filename, source_dir=os.path.join(path, dir_name))
