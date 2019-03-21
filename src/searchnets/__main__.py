a0b316847b437074e3362d3987c38dee877718ab
NickleDave <nicholdav@gmail.com>
2019-03-20 22:57:55 -0400

    move config unpacking from train.py into main.cli

--- src/searchnets/__main__.py
+++ src/searchnets/__main__.py
@@ -57,7 +57,39 @@ def cli(command, configfile):
              gz_filename=gz_filename
              )
     elif command == 'train':
-        train(config)
+        if config.has_option('DATA', 'VALIDATION_SIZE'):
+            val_size = int(config['DATA']['VALIDATION_SIZE'])
+        else:
+            val_size = None
+        gz_filename = config['DATA']['GZ_FILENAME']
+        net_name = config['TRAIN']['NETNAME']
+        number_nets_to_train = int(config['TRAIN']['number_nets_to_train'])
+        input_shape = ast.literal_eval(config['TRAIN']['INPUT_SHAPE'])
+        new_learn_rate_layers = ast.literal_eval(config['TRAIN']['NEW_LEARN_RATE_LAYERS'])
+        base_learning_rate = float(config['TRAIN']['BASE_LEARNING_RATE'])
+        new_layer_learning_rate = float(config['TRAIN']['NEW_LAYER_LEARNING_RATE'])
+        epochs_list = ast.literal_eval(config['TRAIN']['EPOCHS'])
+        batch_size = int(config['TRAIN']['BATCH_SIZE'])
+        random_seed = int(config['TRAIN']['RANDOM_SEED'])
+        if config.has_option('TRAIN', 'DROPOUT_RATE'):
+            dropout_rate = config['TRAIN']['DROPOUT_RATE']
+        else:
+            dropout_rate = 0.5
+        model_save_path = config['TRAIN']['MODEL_SAVE_PATH']
+
+        train(gz_filename=gz_filename,
+              net_name=net_name,
+              number_nets_to_train=number_nets_to_train,
+              input_shape=input_shape,
+              new_learn_rate_layers=new_learn_rate_layers,
+              base_learning_rate=base_learning_rate,
+              new_layer_learning_rate=new_layer_learning_rate,
+              epochs_list=epochs_list,
+              batch_size=batch_size,
+              random_seed=random_seed,
+              model_save_path=model_save_path,
+              dropout_rate=dropout_rate,
+              val_size=val_size)
     elif command == 'test':
         test(config)
     elif command == 'all':
