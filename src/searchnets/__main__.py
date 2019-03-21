9cf884a6afaa51e7214509cae7c25cd633f65f74
NickleDave <nickledave@users.noreply.github.com>
2019-03-21 15:41:33 -0400

    refactor __main__ so cli uses new config class, obeys DRY

--- src/searchnets/__main__.py
+++ src/searchnets/__main__.py
@@ -13,9 +13,51 @@ from .train import train
 from .test import test
 
 
+def _call_data(config):
+    """helper function to call searchstims.data
+    to achieve Don't Repeat Yourself within cli function"""
+    data(train_dir=config.data.train_dir,
+         train_size=config.data.train_size,
+         val_size=config.data.val_size,
+         gz_filename=config.data.gz_filename
+         )
+
+
+def _call_train(config):
+    """helper function to call searchstims.train
+    to achieve Don't Repeat Yourself within cli function"""
+    train(gz_filename=config.data.gz_filename,
+          net_name=config.train.net_name,
+          number_nets_to_train=config.train.number_nets_to_train,
+          input_shape=config.train.input_shape,
+          new_learn_rate_layers=config.train.new_learn_rate_layers,
+          base_learning_rate=config.train.base_learning_rate,
+          new_layer_learning_rate=config.train.new_layer_learning_rate,
+          epochs_list=config.train.epochs_list,
+          batch_size=config.train.batch_size,
+          random_seed=config.train.random_seed,
+          model_save_path=config.train.model_save_path,
+          dropout_rate=config.train.dropout_rate,
+          val_size=config.data.val_size)
+
+
+def _call_test(config):
+    """helper function to call searchstims.test
+    to achieve Don't Repeat Yourself within cli function"""
+    test(gz_filename=config.data.gz_filename,
+         net_name=config.train.net_name,
+         number_nets_to_train=config.train.number_nets_to_train,
+         input_shape=config.train.input_shape,
+         new_learn_rate_layers=config.train.new_learn_rate_layers,
+         epochs_list=config.train.epochs_list,
+         batch_size=config.train.batch_size,
+         model_save_path=config.train.model_save_path,
+         test_results_save_path=config.test.test_results_save_path)
+
+
 def cli(command, configfile):
     """command-line interface
-    Called by main() when user runs ram from the command-line by typing 'searchnets'
+    Called by main() when user runs from the command-line by typing 'searchnets'
 
     Parameters
     ----------
@@ -44,58 +86,18 @@ def cli(command, configfile):
     config = parse_config(configfile)
 
     if command == 'data':
-        train_size = int(config['DATA']['TRAIN_SIZE'])
-        if config.has_option('DATA', 'VALIDATION_SIZE'):
-            val_size = int(config['DATA']['VALIDATION_SIZE'])
-        else:
-            val_size = None
-        gz_filename = config['DATA']['GZ_FILENAME']
+        _call_data(config)
 
-        data(train_dir=config['DATA']['TRAIN_DIR'],
-             train_size=train_size,
-             val_size=val_size,
-             gz_filename=gz_filename
-             )
     elif command == 'train':
-        if config.has_option('DATA', 'VALIDATION_SIZE'):
-            val_size = int(config['DATA']['VALIDATION_SIZE'])
-        else:
-            val_size = None
-        gz_filename = config['DATA']['GZ_FILENAME']
-        net_name = config['TRAIN']['NETNAME']
-        number_nets_to_train = int(config['TRAIN']['number_nets_to_train'])
-        input_shape = ast.literal_eval(config['TRAIN']['INPUT_SHAPE'])
-        new_learn_rate_layers = ast.literal_eval(config['TRAIN']['NEW_LEARN_RATE_LAYERS'])
-        base_learning_rate = float(config['TRAIN']['BASE_LEARNING_RATE'])
-        new_layer_learning_rate = float(config['TRAIN']['NEW_LAYER_LEARNING_RATE'])
-        epochs_list = ast.literal_eval(config['TRAIN']['EPOCHS'])
-        batch_size = int(config['TRAIN']['BATCH_SIZE'])
-        random_seed = int(config['TRAIN']['RANDOM_SEED'])
-        if config.has_option('TRAIN', 'DROPOUT_RATE'):
-            dropout_rate = config['TRAIN']['DROPOUT_RATE']
-        else:
-            dropout_rate = 0.5
-        model_save_path = config['TRAIN']['MODEL_SAVE_PATH']
+        _call_train(config)
 
-        train(gz_filename=gz_filename,
-              net_name=net_name,
-              number_nets_to_train=number_nets_to_train,
-              input_shape=input_shape,
-              new_learn_rate_layers=new_learn_rate_layers,
-              base_learning_rate=base_learning_rate,
-              new_layer_learning_rate=new_layer_learning_rate,
-              epochs_list=epochs_list,
-              batch_size=batch_size,
-              random_seed=random_seed,
-              model_save_path=model_save_path,
-              dropout_rate=dropout_rate,
-              val_size=val_size)
     elif command == 'test':
-        test(config)
+        _call_test(config)
+
     elif command == 'all':
-        data(config)
-        train(config)
-        test(config)
+        _call_data(config)
+        _call_train(config)
+        _call_test(config)
 
 
 def get_parser():
