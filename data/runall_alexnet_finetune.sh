402d48cd581d8673137c026f166da12b952aac50
NickleDave <nickledave@users.noreply.github.com>
2019-05-23 21:02:43 -0400

    bugfix in runall_alenxet_finetune.sh, add runall_vgg16_finetune.sh

--- data/runall_alexnet_finetune.sh
+++ data/runall_alexnet_finetune.sh
@@ -2,9 +2,5 @@
 
 # prepare data sets for training neural nets, then train, then test accuracy of trained nets
 searchnets all ./configs/searchnets_alexnet_finetune_RVvGV.ini
-searchnets all ./configs/searchnets_alexnet_finetune_RVvRGHGV.ini
+searchnets all ./configs/searchnets_alexnet_finetune_RVvRHGV.ini
 searchnets all ./configs/searchnets_alexnet_finetune_2_v_5.ini
-
-searchnets all ./configs/searchnets_vgg16_finetune_RVvGV.ini
-searchnets all ./configs/searchnets_vgg16_finetune_RVvRHGV.ini
-searchnets all ./configs/searchnets_vgg16_finetune_2_v_5.ini
