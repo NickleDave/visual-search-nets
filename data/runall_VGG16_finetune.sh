5a98ae5a9bdf742cf11dd7c9e8051fae1920d85b
NickleDave <nickledave@users.noreply.github.com>
2019-05-26 20:15:12 -0400

    update runall_VGG16_finetune.sh

--- data/runall_VGG16_finetune.sh
+++ data/runall_VGG16_finetune.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # prepare data sets for training neural nets, then train, then test accuracy of trained nets
-searchnets all ./configs/searchnets_vgg16_finetune_RVvGV.ini
-searchnets all ./configs/searchnets_vgg16_finetune_RVvRHGV.ini
-searchnets all ./configs/searchnets_vgg16_finetune_2_v_5.ini
+searchnets all ./configs/searchnets_VGG16_finetune_RVvGV.ini
+searchnets all ./configs/searchnets_VGG16_finetune_RVvRHGV.ini
+searchnets all ./configs/searchnets_VGG16_finetune_2_v_5.ini
