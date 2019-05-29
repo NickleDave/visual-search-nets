71ce12bad6a32e931bab4e15bbffe53070b97516
NickleDave <nickledave@users.noreply.github.com>
2019-05-29 13:11:11 -0400

    add other learncurve configs to runall_alexnet_learncurve_2_v_5.sh

--- data/runall_alexnet_learncurve_2_v_5.sh
+++ data/runall_alexnet_learncurve_2_v_5.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-# prepare data sets for training neural nets, then train, then test accuracy of trained nets
+searchnets data ./configs/searchnets_alexnet_learncurve_RVvGV.ini
+searchnets learncurve ./configs/searchnets_alexnet_learncurve_RVvGV.ini
+
+searchnets data ./configs/searchnets_alexnet_learncurve_RVvRHGV.ini
+searchnets learncurve ./configs/searchnets_alexnet_learncurve_RVvRHGV.ini
+
 searchnets data ./configs/searchnets_alexnet_learncurve_2_v_5.ini
 searchnets learncurve ./configs/searchnets_alexnet_learncurve_2_v_5.ini
