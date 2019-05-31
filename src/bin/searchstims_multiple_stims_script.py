d479d14b76e75e787c5b81f073b5fc89678a6449
NickleDave <nickledave@users.noreply.github.com>
2019-05-31 11:24:42 -0400

    add script that creates dataset with all 3 stim types

---
+++ src/bin/searchstims_multiple_stims_script.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+
+import searchstims.make
+from searchstims.stim_makers import RVvGVStimMaker, RVvRHGVStimMaker, Two_v_Five_StimMaker
+
+ALEXNET_SIZE = (227, 227)
+VGG16_SIZE = (224, 224)
+BORDER_SIZE = (30, 30)
+GRID_SIZE = (5, 5)
+ITEM_BBOX_SIZE = (30, 30)
+JITTER = 12
+
+for window_size in ALEXNET_SIZE, VGG16_SIZE:
+    keys = ['RVvGV', 'RVvRHGV', '2_v_5']
+    vals = [
+        RVvGVStimMaker(target_color='red',
+                       distractor_color='green',
+                       window_size=window_size,
+                       border_size=BORDER_SIZE,
+                       grid_size=GRID_SIZE,
+                       item_bbox_size=ITEM_BBOX_SIZE,
+                       jitter=JITTER),
+        RVvRHGVStimMaker(target_color='red',
+                         distractor_color='green',
+                         window_size=window_size,
+                         border_size=BORDER_SIZE,
+                         grid_size=GRID_SIZE,
+                         item_bbox_size=ITEM_BBOX_SIZE,
+                         jitter=JITTER),
+        Two_v_Five_StimMaker(target_color='white',
+                             distractor_color='white',
+                             window_size=window_size,
+                             border_size=BORDER_SIZE,
+                             grid_size=GRID_SIZE,
+                             item_bbox_size=ITEM_BBOX_SIZE,
+                             jitter=JITTER,
+                             target_number=2,
+                             distractor_number=5)
+    ]
+    if window_size == ALEXNET_SIZE:
+        alexnet_zip = zip(keys, vals)
+    elif window_size == VGG16_SIZE:
+        vgg16_zip = zip(keys, vals)
+
+OUTPUT_DIR = Path('data/visual_search_stimuli')
+TARGET_PRESENT = 12100
+TARGET_ABSENT = 12100
+SET_SIZES = [1, 2, 4, 8]
+
+
+def main():
+    for cnn, zipped in zip(['alexnet', 'VGG16'], [alexnet_zip, vgg16_zip]):
+        json_filename = f'{cnn}_RVvGV_RVvRHV_2v5.json'
+        stim_dict = dict(zipped)
+        output_dir = OUTPUT_DIR.joinpath(f'{cnn}_RVvGV_RVvRHV_2v5')
+        searchstims.make.make(root_output_dir=output_dir,
+                              stim_dict=stim_dict,
+                              json_filename=json_filename,
+                              num_target_present=TARGET_PRESENT,
+                              num_target_absent=TARGET_ABSENT,
+                              set_sizes=SET_SIZES)
+
+
+if __name__ == '__main__':
+    main()
