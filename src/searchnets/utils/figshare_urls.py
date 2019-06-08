1e146e7bdd69e501f9e09a5b36d061ad00cb127c
NickleDave <nickledave@users.noreply.github.com>
2019-06-08 14:35:53 -0400

    fix URLs to neural net weights on figshare

--- src/searchnets/utils/figshare_urls.py
+++ src/searchnets/utils/figshare_urls.py
@@ -1,2 +1,2 @@
-ALEXNET_WEIGHTS_URL = 'https://ndownloader.figshare.com/files/14299136'
-VGG16_WEIGHTS_URL = 'https://ndownloader.figshare.com/files/14449532'
+ALEXNET_WEIGHTS_URL = 'https://ndownloader.figshare.com/files/15375197'
+VGG16_WEIGHTS_URL = 'https://ndownloader.figshare.com/files/15375212'
