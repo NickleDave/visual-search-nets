63ccce569d4316f4a8d2f3dc395218ae8de8d74c
NickleDave <nickledave@users.noreply.github.com>
2019-07-08 21:01:12 -0500

    fix plot/__init__.py so it imports metric_v_set_size

--- src/searchnets/plot/__init__.py
+++ src/searchnets/plot/__init__.py
@@ -1,1 +1,2 @@
-from .figures import acc_v_set_size, acc_v_set_size_df, ftr_v_spt_conj, item_heatmap, mn_slope_by_epoch, train_history
+from .figures import acc_v_set_size, ftr_v_spt_conj, item_heatmap, metric_v_set_size_df,\
+                     mn_slope_by_epoch, train_history
