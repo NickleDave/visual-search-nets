795e9ab242a5a729c9495ff0bacf13af6cef4a9f
NickleDave <nickledave@users.noreply.github.com>
2019-08-09 19:53:38 -0400

    add tests for D prime tests/test_metrics.pY

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,6 +1,7 @@
 import unittest
 
 import numpy as np
+import tensorflow as tf
 
 import searchnets
 
@@ -24,6 +25,40 @@ class TestMetrics(unittest.TestCase):
         with self.assertRaises(ValueError):
             searchnets.utils.metrics.p_item_grid(char_grids_multiple_shapes)
 
+    def test_compute_d_prime(self):
+        y_true = np.random.choice(np.asarray([0, 1]), size=(500,))
+        y_pred = np.copy(y_true)
+        hit_rate, false_alarm_rate, d_prime = searchnets.utils.metrics.compute_d_prime(y_true, y_pred)
+        # range of d_prime is, roughly, (-5.7538..., 5.75388...)
+        self.assertTrue(d_prime > 5.75)
+
+        y_pred_not = np.logical_not(y_true).astype(int)
+        hr_not, far_not, d_prime_not = searchnets.utils.metrics.compute_d_prime(y_true, y_pred_not)
+        self.assertTrue(d_prime_not < -5.75)
+
+
+class TestDPrime(tf.test.TestCase):
+    def test_dprime(self):
+        N_SAMPLES = 500
+        with self.test_session() as sess:
+            y_true_ph = tf.placeholder(dtype=tf.int32, shape=(N_SAMPLES,))
+            y_pred_ph = tf.placeholder(dtype=tf.int32, shape=(N_SAMPLES,))
+            y_true = np.random.choice(
+                a=np.asarray([0, 1]),
+                size=(500,),
+            ).astype(np.int32)
+            y_pred = np.copy(y_true)
+
+            y_true = tf.convert_to_tensor(y_true)
+            y_pred = tf.convert_to_tensor(y_pred)
+
+            d_prime = sess.run(
+                searchnets.utils.metrics.d_prime_tf(y_true, y_pred),
+                feed_dict={y_true_ph: y_true, y_pred_ph: y_pred}
+            )
+            self.assertTrue(d_prime > 5.75)
+
 
 if __name__ == '__main__':
     unittest.main()
+    tf.test.TestCase.main()
