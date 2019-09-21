173ab5a914e6d01aa46b8e719e8d88c88d6d72e5
NickleDave <nickledave@users.noreply.github.com>
2019-09-21 12:50:24 -0400

    modify multiple_stims script for new experiments
    - add two new stimuli, that are like RVvGV and RVvRHGV, except colors are Cyan and Papaya Whip
      + since these colors require multiple RGB channels, they (hopefully) cannot be solved trivially
    - other minor changes required for newest version of searchstims
      + e.g., use 'csv_filename' instead of 'json_filename'

--- src/scripts/searchstims_multiple_stims_script.py
+++ src/scripts/searchstims_multiple_stims_script.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import searchstims.make
 from searchstims.stim_makers import RVvGVStimMaker, RVvRHGVStimMaker, Two_v_Five_StimMaker, TLStimMaker, xoStimMaker, TStimMaker
 
+HERE = Path(__file__).parent
+
 ALEXNET_SIZE = (227, 227)
 VGG16_SIZE = (224, 224)
 BORDER_SIZE = (30, 30)
@@ -11,7 +13,8 @@ ITEM_BBOX_SIZE = (30, 30)
 JITTER = 12
 
 for window_size in ALEXNET_SIZE, VGG16_SIZE:
-    keys = ['RVvGV', 'RVvRHGV', '2_v_5', 'YT_v_BTYL', 'YT_v_BTBL', 'Bx_v_RxBo', 'Bx_v_RxRo', 'TvT']
+    keys = ['RVvGV', 'RVvRHGV', 'PWVvCV', 'PWVvPWHCV', '2_v_5', 'YT_v_BTYL',
+            'YT_v_BTBL', 'Bx_v_RxBo', 'Bx_v_RxRo', 'TvT']
     vals = [
         RVvGVStimMaker(target_color='red',
                        distractor_color='green',
@@ -22,6 +25,20 @@ for window_size in ALEXNET_SIZE, VGG16_SIZE:
                        jitter=JITTER),
         RVvRHGVStimMaker(target_color='red',
                          distractor_color='green',
+                         window_size=window_size,
+                         border_size=BORDER_SIZE,
+                         grid_size=GRID_SIZE,
+                         item_bbox_size=ITEM_BBOX_SIZE,
+                         jitter=JITTER),
+        RVvGVStimMaker(target_color=(255, 239, 213),
+                       distractor_color=(255, 192, 203),
+                       window_size=window_size,
+                       border_size=BORDER_SIZE,
+                       grid_size=GRID_SIZE,
+                       item_bbox_size=ITEM_BBOX_SIZE,
+                       jitter=JITTER),
+        RVvRHGVStimMaker(target_color=(255, 239, 213),
+                         distractor_color=(255, 192, 203),
                          window_size=window_size,
                          border_size=BORDER_SIZE,
                          grid_size=GRID_SIZE,
@@ -74,7 +91,7 @@ for window_size in ALEXNET_SIZE, VGG16_SIZE:
     elif window_size == VGG16_SIZE:
         vgg16_zip = zip(keys, vals)
 
-OUTPUT_DIR = Path('data/visual_search_stimuli')
+OUTPUT_DIR = HERE.joinpath('../../../visual_search_stimuli')  # in a separate folder in same parent dir as source code root
 TARGET_PRESENT = [3600, 7200, 14400, 28800]
 TARGET_ABSENT = [3600, 7200, 14400, 28800]
 SET_SIZES = [1, 2, 4, 8]
@@ -82,12 +99,12 @@ SET_SIZES = [1, 2, 4, 8]
 
 def main():
     for cnn, zipped in zip(['alexnet', 'VGG16'], [alexnet_zip, vgg16_zip]):
-        json_filename = f'{cnn}_train_multiple_stims.json'
+        csv_filename = f'{cnn}_multiple_stims.csv'
         stim_dict = dict(zipped)
         output_dir = OUTPUT_DIR.joinpath(f'{cnn}_multiple_stims')
         searchstims.make.make(root_output_dir=output_dir,
                               stim_dict=stim_dict,
-                              json_filename=json_filename,
+                              csv_filename=csv_filename,
                               num_target_present=TARGET_PRESENT,
                               num_target_absent=TARGET_ABSENT,
                               set_sizes=SET_SIZES)
