13a231b93fe3be5f4491ba1cd2e068977013ce59
NickleDave <nickledave@users.noreply.github.com>
2019-09-23 09:07:09 -0400

    add missing net_name argument in Trainer factory function

--- src/searchnets/classes/trainer.py
+++ src/searchnets/classes/trainer.py
@@ -73,6 +73,6 @@ class Trainer(AbstractTrainer):
         #     ])
         #     loss_op = CE_loss_op + triplet_loss_op
 
-        kwargs = dict(**kwargs, model=model, optimizers=optimizers, criterion=criterion)
+        kwargs = dict(**kwargs, net_name=net_name, model=model, optimizers=optimizers, criterion=criterion)
         trainer = cls(**kwargs)
         return trainer
