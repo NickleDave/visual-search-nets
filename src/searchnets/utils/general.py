ec368ff390fd24808057ed33934eac2242e106f2
NickleDave <nickledave@users.noreply.github.com>
2019-10-07 20:58:21 -0400

    rewrite utils.general.results_df function
    - now works with a single data .csv and a single results .gz file
    - now takes arguments 'method' and 'learning_rate', adds these as columns to output dataframe
    - instead of trying to loop over multiple data/results files programatically, this function just deals
      with one pair at a time
    - another function / script can do the looping

--- src/searchnets/utils/general.py
+++ src/searchnets/utils/general.py
@@ -53,7 +53,8 @@ def targz_dirs(dir_names=('checkpoints', 'configs', 'data_prepd_for_nets', 'resu
 
 
 COLUMNS = ['net_name',
-           'train_type',
+           'method',
+           'learning_rate',
            'net_number',
            'stimulus',
            'set_size',
@@ -65,45 +66,30 @@ COLUMNS = ['net_name',
            ]
 
 
-def results_df(data_prep_dir,
-               results_dir,
-               nets=('alexnet', 'VGG16'),
-               train_types=('finetune', 'train'),
-               stims=('2_v_5', 'RVvGV', 'RVvRHGV'),
-               target_condition=('present', 'absent', 'both'),
-               data_gz_paths=None,
-               results_gz_paths=None,
-               csv_path=None,
+def results_df(data_csv_path,
+               results_gz_path,
+               net_name,
+               method,
+               learning_rate,
+               results_csv_path=None,
                ):
     """creates Pandas dataframe from results.
     The resulting dataframe can be used with searchstims.plot.figures.metric_v_set_size_df
 
     Parameters
     ----------
-    data_prep_dir : pathlib.Path
-        used to find paths to .gz files containing results,
-        if not supplied via data_gz_paths argument
-    results_dir : pathlib.Path
-        used to find paths to .gz files containing results,
-        if not supplied via results_gz_paths argument
-    nets : list
-        of string, names of neural network architectures.
-        Default is ('alexnet', 'VGG16')
-    train_types : list
-        of string, training methods used. Expected to be in data and results filenames.
-        Default is ('finetune', 'train')
-    stims : list
-        of str, names of visual search stimuli.
-        Default is ('2_v_5', 'RVvGV', 'RVvRHGV').
-    target_condition : list
-        of str, valid options are {'present', 'absent', 'both'}.
-        Conditions for which to compute metrics (e.g. accuracy).
-        Default is all three, ['present', 'absent', 'both'].
-    data_gz_paths : list
-        of str, paths to data files. In case they have different names than what would be determined programatically.
-    results_gz_paths
-        of str, paths to results files. In case they have different names than what would be determined programatically.
-    csv_path : str
+    data_csv_path : str
+        path to .csv file of dataset splits, created by running 'searchnets split' from the command line.
+    results_gz_path
+        paths to .gz file created by running 'searchnets test' from the command line.
+    net_name : str
+        name of convolutional neural net architecture to train.
+        One of {'alexnet', 'VGG16'}
+    method : str
+        training method. One of {'initialize', 'transfer'}. See docstring for searchnets.train.
+    learning_rate : float
+        hyperparameter used during weight update step of training network.
+    results_csv_path : str
         Path to use to save dataframe as a csv.
         Default is None, in which case no csv is saved.
 
@@ -112,76 +98,58 @@ def results_df(data_prep_dir,
     df : pandas.Dataframe
         computed from results
     """
-    if results_gz_paths and data_gz_paths:
-        if len(results_gz_paths) != len(data_gz_paths):
-            raise ValueError(
-                'results_gz_paths must be same length as data_gz_paths'
-            )
+    df_dataset = pd.read_csv(data_csv_path)
+    df_testset = df_dataset[df_dataset['split'] == 'test']
+    # add y_true column to df_testset that we compare to y_pred
+    # (can't compare y_pred to target_condition column since that is text,
+    # not integer like output of network)
+    df_testset['y_true'] = df_testset['target_condition'] == 'present'
+    set_sizes = df_testset['set_size'].unique()
 
-        num_iter_main_loop = len(train_types) * len(nets) * len(stims)
-        if len(results_gz_paths) != num_iter_main_loop:
-            raise ValueError(
-                f'not enough paths in results_gz_paths (length {len(results_gz_paths)} '
-                f'for all iterations of main loop, {num_iter_main_loop}.'
-            )
+    stims = df_testset['stimulus'].unique().tolist()
 
-    iter_counter = 0
+    results_dict = joblib.load(results_gz_path)
+
     rows = []
-    for train_type in train_types:
-        for net in nets:
-            for stim in stims:
-                if data_gz_paths:
-                    data_gz_path = data_gz_paths[iter_counter]
-                else:
-                    data_gz_path = data_prep_dir.joinpath(f'{net}_{train_type}_{stim}_data.gz')
 
-                data_dict = joblib.load(data_gz_path)
-                y_test = data_dict['y_test']
-                set_size_vec_test = data_dict['set_size_vec_test']
-                set_sizes = data_dict['set_sizes_by_stim_type'][stim]
+    preds_per_model = results_dict['predictions_per_model_dict']
+    num_nets = len(preds_per_model.keys())
+    for net_num in range(1, num_nets + 1):
+        # below, notice we need to convert key (a Path) to string to check if net_num is 'in' it
+        key = [key for key in preds_per_model.keys() if f'net_number_{net_num}' in str(key)][0]
+        # add y_pred column to test set dataframe
+        # notice we overwrite this column every time through the loop, i.e. for each trained network
+        df_testset['y_pred'] = preds_per_model[key]
 
-                if results_gz_paths:
-                    results_gz_path = results_gz_paths[iter_counter]
-                else:
-                    results_gz_path = list(results_dir.joinpath(f'{net}_{train_type}_{stim}').glob(f'*test*.gz'))
-                    assert len(results_gz_path) == 1, 'found more than one results_gz_path with glob'
-                    results_gz_path = results_gz_path[0]
+        for stim in stims:
+            stim_df = df_testset[df_testset['stimulus'] == stim]
 
-                results_dict = joblib.load(results_gz_path)
-                ppm = results_dict['predictions_per_model_dict']
-                num_nets = len(ppm.keys())
-                for net_num in range(1, num_nets + 1):
-                    # below, notice we need to convert key (a Path) to string to check if net_num is 'in' it
-                    key = [key for key in ppm.keys() if f'net_number_{net_num}' in str(key)][0]
-                    y_pred = ppm[key]
-                    for target_cond in target_condition:
-                        for set_size in set_sizes:
-                            if target_cond == 'present':
-                                inds_this_cond = np.where(
-                                    np.logical_and(y_test == 1, set_size_vec_test == set_size))
-                            elif target_cond == 'absent':
-                                inds_this_cond = np.where(
-                                    np.logical_and(y_test == 0, set_size_vec_test == set_size))
-                            elif target_cond == 'both':
-                                inds_this_cond = np.where(set_size_vec_test == set_size)[0]
-                            acc = np.sum(y_pred[inds_this_cond] == y_test[inds_this_cond])
-                            acc = acc / y_test[inds_this_cond].shape[0]
+            for target_cond in ('present', 'absent', 'both'):
+                for set_size in set_sizes:
+                    set_size_df = stim_df[stim_df['set_size'] == set_size]
+                    if target_cond == 'present':
+                        cond_df = set_size_df[set_size_df['y_true'] == 1]
+                    elif target_cond == 'absent':
+                        cond_df = set_size_df[set_size_df['y_true'] == 0]
+                    elif target_cond == 'both':
+                        cond_df = set_size_df
+                    correct_bool = cond_df['y_true'] == cond_df['y_pred']
+                    import pdb;pdb.set_trace()
+                    acc = np.sum(correct_bool) / correct_bool.shape[0]
 
-                            if target_cond == 'both':
-                                hit_rate, false_alarm_rate, d_prime = compute_d_prime(
-                                    y_pred[inds_this_cond], y_test[inds_this_cond]
-                                )
-                            else:
-                                hit_rate, false_alarm_rate, d_prime = None, None, None
-                            row = [net, train_type, net_num, stim, set_size, target_cond,
-                                   acc, hit_rate, false_alarm_rate, d_prime]
-                            rows.append(row)
-
-                iter_counter += 1
+                    if target_cond == 'both':
+                        hit_rate, false_alarm_rate, d_prime = compute_d_prime(
+                            cond_df['target_condition'].to_numpy(), cond_df['y_pred'].to_numpy()
+                        )
+                    else:
+                        hit_rate, false_alarm_rate, d_prime = None, None, None
+                    row = [net_name, method, learning_rate, net_num, stim, set_size, target_cond,
+                           acc, hit_rate, false_alarm_rate, d_prime]
+                    rows.append(row)
 
     df = pd.DataFrame.from_records(rows, columns=COLUMNS)
-    if csv_path:
-        df.to_csv(csv_path)
+    if results_csv_path:
+        df.to_csv(results_csv_path)
     return df
 
 
