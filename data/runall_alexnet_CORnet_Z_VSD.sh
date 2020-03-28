0f48f9f9f842ebbc7084d45a0b0fc19aa36bcb48
David Nicholson <NickleDave@users.noreply.github.com>
2020-03-28 17:22:19 -0400

    organize / add VSD configs

--- data/runall_alexnet_CORnet_Z_VSD.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-searchnets train ./configs/VSD_alexnet_transfer_lr_1e-03_no_finetune.ini
-searchnets test ./configs/VSD_alexnet_transfer_lr_1e-03_no_finetune.ini
-
-searchnets train ./configs/VSD_CORnet_Z_transfer_lr_1e-03_no_finetune.ini
-searchnets test ./configs/VSD_CORnet_Z_transfer_lr_1e-03_no_finetune.ini
