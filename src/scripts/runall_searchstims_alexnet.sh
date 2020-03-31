0957d4f4f87aeccaf087becd70ebd16bad919d31
NickleDave <nickledave@users.noreply.github.com>
2020-03-31 09:36:19 -0400

    fixup runall

--- src/scripts/runall_searchstims_alexnet.sh
+++ src/scripts/runall_searchstims_alexnet.sh
@@ -1,7 +1,7 @@
 # script should be run from root of project, paths are written relative to root
 alexnet_ini=./data/configs/searchstims_experiments/searchstims_alexnet*ini
 
-for file in $alexnet_ini};do
+for file in ${alexnet_ini};do
 	searchnets train ${file}
 	searchnets test ${file}
 done
