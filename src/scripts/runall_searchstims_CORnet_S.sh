c9ca908d46d14dc70b5f5d2883d26012f45c235a
NickleDave <nickledave@users.noreply.github.com>
2020-05-13 17:11:52 -0400

    add four VSD and searchtims runall bash scripts

---
+++ src/scripts/runall_searchstims_CORnet_S.sh
@@ -0,0 +1,7 @@
+# script should be run from root of project, paths are written relative to root
+CORnet_S_ini=./data/configs/searchstims_experiments/searchstims_CORnet_S*ini
+
+for file in ${CORnet_S_ini};do
+	searchnets train ${file}
+	searchnets test ${file}
+done
